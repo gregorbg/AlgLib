048288637ea5da9eff269539649e29e7add8ab07
suushie_maniac <suushiemaniac@gmail.com>
2015-12-23 16:18:28 +0100

    Fixed bugs and implemented SubGroup comparability

--- src/com/suushiemaniac/cubing/alglib/util/SubGroup.java
+++ src/com/suushiemaniac/cubing/alglib/util/SubGroup.java
@@ -2,33 +2,60 @@ package com.suushiemaniac.cubing.alglib.util;
 
 import com.suushiemaniac.cubing.alglib.alg.Algorithm;
 import com.suushiemaniac.cubing.alglib.move.plane.CubicPlane;
-import com.suushiemaniac.cubing.alglib.move.plane.Plane;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class SubGroup implements StringFormat {
+    private class GroupComparator implements Comparator<CubicPlane> {
+        @Override
+        public int compare(CubicPlane o1, CubicPlane o2) {
+            CubicPlane[] orderedPlanes = {CubicPlane.MIDDLE, CubicPlane.SANDWICH, CubicPlane.EQUATOR, CubicPlane.LEFT, CubicPlane.RIGHT, CubicPlane.UP, CubicPlane.DOWN, CubicPlane.FRONT, CubicPlane.BACK};
+            int o1Index = 0;
+            for (int i = 0; i < orderedPlanes.length; i++) if (orderedPlanes[i] == o1) o1Index = i;
+            int o2Index = 0;
+            for (int i = 0; i < orderedPlanes.length; i++) if (orderedPlanes[i] == o2) o2Index = i;
+
+            if (o1Index < o2Index) return -1;
+            else if (o1Index == o2Index) return 0;
+            else return 1;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof GroupComparator)) return false;
+            for (CubicPlane p1 : CubicPlane.values())
+                for (CubicPlane p2 : CubicPlane.values())
+                    if (this.compare(p1, p2) != ((GroupComparator) obj).compare(p1, p2))
+                        return false;
+            return true;
+        }
+    }
+
     public static SubGroup fromAlg(Algorithm alg) {
-        Plane[] algPlanes = new Plane[alg.allMoves().length];
+        CubicPlane[] algPlanes = new CubicPlane[alg.allMoves().length];
         for (int i = 0; i < alg.allMoves().length; i++)
-            algPlanes[i] = alg.nMove(i).getPlane();
+            algPlanes[i] = (CubicPlane) alg.nMove(i).getPlane();
         return new SubGroup(algPlanes);
     }
 
-    public static SubGroup fromCubicAlgString(String alg) {
+    public static SubGroup fromCubicGroupString(String alg) {
         String[] parts = alg.replace("<", "").replace(">", "").split(",");
-        Plane[] algPlanes = new Plane[parts.length];
+        CubicPlane[] algPlanes = new CubicPlane[parts.length];
         for (int i = 0; i < parts.length; i++)
             algPlanes[i] = CubicPlane.fromNotation(parts[i]);
         return new SubGroup(algPlanes);
     }
 
-    private ArrayList<Plane> groupList;
+    private ArrayList<CubicPlane> groupList;
 
-    public SubGroup(Plane... planes) {
+    public SubGroup(CubicPlane... planes) {
         this.groupList = new ArrayList<>();
-        for (Plane p : planes)
-            if (this.groupList.contains(p))
+        for (CubicPlane p : planes)
+            if (!this.groupList.contains(p))
                 this.groupList.add(p);
+        Collections.sort(this.groupList, new GroupComparator());
     }
 
     private String[] getElementsAsString() {
@@ -45,7 +72,23 @@ public class SubGroup implements StringFormat {
     }
 
     public boolean sameSubGroup(SubGroup other) {
-        return this.size() == other.size() && this.containsAll(other);
+        return this.size() == other.size() && this.containsAll(other) && other.containsAll(this);
+    }
+
+    public boolean smallerSubGroup(SubGroup other) {
+        return this.size() < other.size() && other.containsAll(this);
+    }
+
+    public boolean sameOrSmallerSubGroup(SubGroup other) {
+        return this.sameSubGroup(other) || this.smallerSubGroup(other);
+    }
+
+    public boolean largerSubGroup(SubGroup other) {
+        return this.size() > other.size() && this.containsAll(other);
+    }
+
+    public boolean sameOrLargerSubGroup(SubGroup other) {
+        return this.sameSubGroup(other) || this.largerSubGroup(other);
     }
 
     public int size() {
