110f6d2205b7a49f1953fae494b0bbbbdd179b8e
suushie_maniac <suushiemaniac@gmail.com>
2016-03-04 00:01:00 +0100

    Fully implemented cubicRotate. Primitive solution but works!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import com.suushiemaniac.cubing.alglib.alg.Algorithm;
 import com.suushiemaniac.cubing.alglib.lang.CubicAlgorithmReader;
 import com.suushiemaniac.cubing.alglib.transform.mirror.CubicMirror;
+import com.suushiemaniac.cubing.alglib.transform.rotate.CubicRotation;
 
 public class Main {
     public static void main(String[] args) {
-        Algorithm testAlg = new CubicAlgorithmReader().parse("U D L R F B x y z M S E");
-        System.out.println(testAlg.transform(CubicMirror.MIRROR_M).toFormatString());
-        System.out.println(testAlg.transform(CubicMirror.MIRROR_S).toFormatString());
-        System.out.println(testAlg.transform(CubicMirror.MIRROR_E).toFormatString());
+        Algorithm testAlg = new CubicAlgorithmReader().parse("R U2 R'");
+        System.out.println(testAlg.transform(CubicMirror.M).toFormatString());
+        System.out.println(testAlg.transform(CubicMirror.S).toFormatString());
+        System.out.println(testAlg.transform(CubicMirror.E).toFormatString());
+        System.out.println();
+        System.out.println(testAlg.transform(CubicRotation.X_90).toFormatString());
+        System.out.println(testAlg.transform(CubicRotation.Y_90).toFormatString());
+        System.out.println(testAlg.transform(CubicRotation.Z_90).toFormatString());
+        System.out.println();
+        System.out.println(testAlg.transform(CubicRotation.Y_270).transform(CubicMirror.S).toFormatString());
     }
 }
--- src/com/suushiemaniac/cubing/alglib/transform/rotate/CubicRotation.java
+++ src/com/suushiemaniac/cubing/alglib/transform/rotate/CubicRotation.java
@@ -1,13 +1,43 @@
 package com.suushiemaniac.cubing.alglib.transform.rotate;
 
+import com.suushiemaniac.cubing.alglib.move.CubicMove;
 import com.suushiemaniac.cubing.alglib.move.Move;
+import com.suushiemaniac.cubing.alglib.move.plane.CubicPlane;
+import com.suushiemaniac.cubing.alglib.util.ArrayUtils;
+
+import static com.suushiemaniac.cubing.alglib.move.plane.CubicPlane.*;
 
 public enum CubicRotation implements Rotation {
-    ;
+    Y_90, X_90, Z_90, Y_180, X_180, Z_180, Y_270, X_270, Z_270;
+
+    private static CubicPlane[][][] rotOrder = {
+            {{RIGHT, FRONT, LEFT, BACK}, {RIGHT_SLICE, FRONT_SLICE, LEFT_SLICE, BACK_SLICE}, {MIDDLE, SANDWICH}, {HORIZONTAL, SPATIAL}},
+            {{UP, BACK, DOWN, FRONT}, {UP_SLICE, BACK_SLICE, DOWN_SLICE, FRONT_SLICE}, {SANDWICH, EQUATOR}, {VERTICAL, SPATIAL}},
+            {{RIGHT, DOWN, LEFT, UP}, {RIGHT_SLICE, DOWN_SLICE, LEFT_SLICE, UP_SLICE}, {MIDDLE, EQUATOR}, {VERTICAL, HORIZONTAL}}
+    };
 
     @Override
     public Move rotate(Move origin) {
-        return null;
+        if (!(origin instanceof CubicMove)) return null;
+        else {
+            int rotBaseOrdinal = this.ordinal() % 3;
+            int rotAmountOrdinal = this.ordinal() / 3;
+
+            CubicMove cOrigin = (CubicMove) origin;
+
+            CubicPlane cPlane = cOrigin.getPlane();
+            int ordinal = cPlane.ordinal();
+            int base = ordinal < 6 ? 0 : ordinal < 12 ? 6 : ordinal < 15 ? 12 : 15;
+            int size = base < 12 ? 6 : 3;
+            boolean isPlaneAffected = rotBaseOrdinal != (ordinal - base) / (size / 3);
+
+            int typeBase = ordinal < 6 ? 0 : ordinal < 12 ? 1 : ordinal < 15 ? 2 : 3;
+            int rotIndex = isPlaneAffected ? ArrayUtils.arrayIndex(rotOrder[rotBaseOrdinal][typeBase], cPlane) : 0;
+            int modLen = (size / 3) * 2;
+            CubicPlane plane = isPlaneAffected ? rotOrder[rotBaseOrdinal][typeBase][(rotIndex + rotAmountOrdinal + 1) % modLen] : cPlane;
+
+            return new CubicMove(plane, cOrigin.getModifier(), cOrigin.getDepth());
+        }
     }
 
     @Override
