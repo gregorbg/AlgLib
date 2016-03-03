87836fea2b3cb3fdf53a667e7e7a10e5f5a18e44
suushie_maniac <suushiemaniac@gmail.com>
2016-03-03 17:02:55 +0100

    Fully implemented cubic mirror

--- src/com/suushiemaniac/cubing/alglib/transform/mirror/CubicMirror.java
+++ src/com/suushiemaniac/cubing/alglib/transform/mirror/CubicMirror.java
@@ -13,7 +13,23 @@ public enum CubicMirror implements Mirror {
         if (!(origin instanceof CubicMove)) return null;
         else {
             CubicMove cOrigin = (CubicMove) origin;
-            return cOrigin;
+
+            CubicPlane cPlane = cOrigin.getPlane();
+            int ordinal = cPlane.ordinal();
+            int base = ordinal < 6 ? 0 : ordinal < 12 ? 6 : ordinal < 15 ? 12 : 15;
+            int size = base < 12 ? 6 : 3;
+            boolean isPlaneAffected = this.ordinal() == (ordinal - base) / (size / 3);
+
+            int offset = isPlaneAffected ? size / 6 : 0;
+            int inTuple = ordinal % (size / 3);
+            int planeIndex = ((inTuple + offset) % (size / 3)) + ordinal - inTuple;
+            CubicPlane plane = CubicPlane.values()[planeIndex];
+
+            boolean isModifierAffected = size == 6 || this.ordinal() != (ordinal - base) / (size / 3);
+            CubicModifier modifier = cOrigin.getModifier();
+            if (isModifierAffected) modifier = modifier.inverse();
+
+            return new CubicMove(plane, modifier, cOrigin.getDepth());
         }
     }
 
